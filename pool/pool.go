9a2d47590a53ab11c5957272475adbc98bf926ed
Brian Picciano <mediocregopher@gmail.com>
2014-07-25 19:13:59 -0400

    added connection pool

---
+++ pool/pool.go
@@ -0,0 +1,54 @@
+package pool
+
+import (
+	"github.com/fzzy/radix/redis"
+)
+
+// A simple connection pool. It will create a small pool of initial connections,
+// and if more connections are needed they will be created on demand. If a
+// connection is returned and the pool is full it will be closed.
+type Pool struct {
+	network string
+	addr    string
+	pool    chan *redis.Client
+}
+
+// Creates a new Pool whose connections are all created using
+// redis.Dial(network, addr). The size indicates the maximum number of idle
+// connections to have waiting to be used at any given moment
+func NewPool(network, addr string, size int) (*Pool, error) {
+	var err error
+	pool := make([]*redis.Client, size)
+	for i := range pool {
+		if pool[i], err = redis.Dial(network, addr); err != nil {
+			return nil, err
+		}
+	}
+	p := Pool{
+		network: network,
+		addr:    addr,
+		pool:    make(chan *redis.Client, len(pool)),
+	}
+	return &p, nil
+}
+
+// Retrieves an available redis client. If there none available it will create a
+// new one on the fly
+func (p *Pool) Get() (*redis.Client, error) {
+	select {
+	case conn := <-p.pool:
+		return conn, nil
+	default:
+		return redis.Dial(p.network, p.addr)
+	}
+}
+
+// Returns a client back to the pool. If the pool is full the client is closed
+// instead
+func (p *Pool) Put(conn *redis.Client) {
+	select {
+	case p.pool<- conn:
+	default:
+		conn.Close()
+	}
+}
---
+++ pool/pool_test.go
@@ -0,0 +1,24 @@
+package pool
+
+import (
+	. "testing"
+	"github.com/fzzy/radix/redis"
+)
+
+func TestPool(t *T) {
+	pool, err := NewPool("tcp", "localhost:6379", 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conns := make([]*redis.Client, 20)
+	for i := range conns {
+		if conns[i], err = pool.Get(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := range conns {
+		pool.Put(conns[i])
+	}
+}
